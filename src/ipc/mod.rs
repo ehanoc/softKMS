e6eee81280014322193c69b491375aede00038f2
ehanoc <ehanoc@protonmail.com>
2026-02-28 20:52:36 +0000

    chore: Deleting unsused folders

--- src/ipc/mod.rs
+++
@@ -1,7 +0,0 @@
-//! IPC module - Inter-process communication
-
-/// IPC types and traits
-pub trait IpcChannel {
-    fn send(&self, data: &[u8]) -> impl std::future::Future<Output = crate::Result<()>> + Send;
-    fn receive(&self) -> impl std::future::Future<Output = crate::Result<Vec<u8>>> + Send;
-}
